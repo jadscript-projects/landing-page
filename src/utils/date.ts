4c1699240215e31d0184474190c448d6cc868fa7
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2026-03-06 11:35:23 -0300

    feat: :sparkles: simplify getWorkingYears function and add JSDoc for better documentation

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,9 @@
+/**
+ * Get the number of years of experience
+ * @returns The number of years of experience
+ */
 export const getWorkingYears = () => {
-    const startDate = new Date("2018-11-01");
-    const currentDate = new Date();
-    const years = currentDate.getFullYear() - startDate.getFullYear();
-    return years;
-  };
+  const startDate = new Date("2018-11-01");
+  const currentDate = new Date();
+  return currentDate.getFullYear() - startDate.getFullYear();
+};
